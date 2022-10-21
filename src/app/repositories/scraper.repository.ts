9fe4da4dbc2ae33dcff5815bd93c33647e5584b7
storresoliv <storresoliv@gmail.com>
2022-10-20 23:24:32 -0300

    feat: add express server

--- src/app/repositories/scraper.repository.ts
+++ src/app/repositories/scraper.repository.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http'
 import { Observable, of } from 'rxjs';
 import { INewEpisodies } from '../models/new-episodies.model';
 import { environment } from 'src/environments/environment';
-import { NEW_EPISODIES } from './scraper.mock';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +12,6 @@ export class ScraperRepository {
   constructor(private readonly http: HttpClient) { }
 
   getNewEpisodies(): Observable<INewEpisodies[]> {
-    // return this.http.get<INewEpisodies[]>(environment.newEpisodies)
-    return of(NEW_EPISODIES)
+    return this.http.get<INewEpisodies[]>(environment.newEpisodies)
   }
 }
